50f3fe0ad65e00ccf33d290a160e78705bb7a5c7
Brandon Seich <blseich@gmail.com>
2025-05-16 09:58:32 -0400

    fix types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import picks from '../data/picks.json';
 
+type Player = {
+  id: string,
+}
+
 const scoreToInt = (score: string) => score === 'E' ? 0 : parseInt(score);
 
 const parseScore = (score: number) => {
@@ -15,7 +19,7 @@ export default async function Home() {
 
   const golfers = render.events[0].competitions[0].competitors;
 
-  const golfersIdMap = golfers.reduce((acc, player) => ({
+  const golfersIdMap = golfers.reduce((acc: Record<string, Player>, player: Player) => ({
     ...acc,
     [player.id]: player,
   }), {});
@@ -30,14 +34,14 @@ export default async function Home() {
       ))} */}
       {picks
         .sort((a_pick, b_pick) => (
-          Object.values(a_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0) -  Object.values(b_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0)
+          Object.values(a_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0) -  Object.values(b_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0)
         ))
         .map((pick, i) => (
-        <div className="mb-8">
+        <div className="mb-8" key={`${pick.id}-${pick.name.replace(' ', '-')}`}>
           <div className='flex gap-4 p-4 bg-emerald-400 text-black font-bold rounded-t-2xl'>
             <div className="shrink-0 grow-0">{i + 1}</div>
             <div className='text-center flex-grow'>{pick.name}</div>
-            <div className="shrink-0 grow-0">Overall: {parseScore(Object.values(pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0))}</div>
+            <div className="shrink-0 grow-0">Overall: {parseScore(Object.values(pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0))}</div>
           </div>
           <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 border-2 border-emerald-950">
             <div className="bg-emerald-800 text-white">
@@ -56,13 +60,13 @@ export default async function Home() {
           <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 [&>div]:bg-white  rounded-b-2xl border-2 border-emerald-950 overflow-hidden">
               {Object
                 .values(pick.picks)
-                .sort((a_id, b_id) => scoreToInt(golfersIdMap[a_id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue) - scoreToInt(golfersIdMap[b_id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue))
+                .sort((a_id, b_id) => scoreToInt(golfersIdMap[a_id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue) - scoreToInt(golfersIdMap[b_id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue))
                 .map((id) => (
                   <>
                     <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].status.position.displayName}</div>
                     <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].athlete.displayName}</div>
                     <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].status.todayDetail || golfersIdMap[id].status.detail}</div>
-                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue}</div>
+                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue}</div>
                   </>
               ))}
           </div>
