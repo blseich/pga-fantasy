824f96d76de19726e0c7e0b010c99883fdf85a5d
Brandon Seich <blseich@gmail.com>
2025-05-16 09:50:53 -0400

    v0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+import picks from '../data/picks.json';
 
-export default function Home() {
+const scoreToInt = (score: string) => score === 'E' ? 0 : parseInt(score);
+
+const parseScore = (score: number) => {
+  if (score === 0) return "E";
+  if (score > 0) return `+${score}`;
+  if (score < 0) return `${score}`;
+  return 0;
+}
+
+export default async function Home() {
+  const result = await fetch('https://site.web.api.espn.com/apis/site/v2/sports/golf/leaderboard?league=pga&region=us&lang=en&event=401703511');
+  const render = await result.json();
+
+  const golfers = render.events[0].competitions[0].competitors;
+
+  const golfersIdMap = golfers.reduce((acc, player) => ({
+    ...acc,
+    [player.id]: player,
+  }), {});
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="p-4">
+      {/* {golfers.map((player) => (
+        <div className="flex">
+          <div>{player.score}</div>
+          <div>{player.athlete.fullName}</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))} */}
+      {picks
+        .sort((a_pick, b_pick) => (
+          Object.values(a_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0) -  Object.values(b_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0)
+        ))
+        .map((pick, i) => (
+        <div className="mb-8">
+          <div className='flex gap-4 p-4 bg-emerald-400 text-black font-bold rounded-t-2xl'>
+            <div className="shrink-0 grow-0">{i + 1}</div>
+            <div className='text-center flex-grow'>{pick.name}</div>
+            <div className="shrink-0 grow-0">Overall: {parseScore(Object.values(pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue), 0))}</div>
+          </div>
+          <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 border-2 border-emerald-950">
+            <div className="bg-emerald-800 text-white">
+                Rank
+              </div>
+              <div className="bg-emerald-800 text-white">
+                Golfer
+              </div>
+              <div className="bg-emerald-800 text-white bl-">
+                Today
+              </div>
+              <div className="bg-emerald-800 text-white">
+                Overall
+              </div>
+          </div>
+          <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 [&>div]:bg-white  rounded-b-2xl border-2 border-emerald-950 overflow-hidden">
+              {Object
+                .values(pick.picks)
+                .sort((a_id, b_id) => scoreToInt(golfersIdMap[a_id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue) - scoreToInt(golfersIdMap[b_id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue))
+                .map((id) => (
+                  <>
+                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].status.position.displayName}</div>
+                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].athlete.displayName}</div>
+                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].status.todayDetail || golfersIdMap[id].status.detail}</div>
+                    <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].statistics.find((stat) => stat.name === 'scoreToPar').displayValue}</div>
+                  </>
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
