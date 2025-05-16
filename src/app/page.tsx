ea5cbd4d54bd56f0ef206949378911ae128233e4
Brandon Seich <blseich@gmail.com>
2025-05-16 11:37:05 -0400

    only add top three scoring golfers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,26 @@ import picks from '../data/picks.json';
 
 type Player = {
   id: string,
+  statistics: {
+    name: string,
+    displayName: string,
+    value: 0,
+  }[]
 }
 
-const scoreToInt = (score: string) => score === 'E' ? 0 : parseInt(score);
-
-const parseScore = (score: number) => {
-  if (score === 0) return "E";
-  if (score > 0) return `+${score}`;
-  if (score < 0) return `${score}`;
-  return 0;
+type Picker = {
+  id: number,
+  name: string,
+  picks: {
+    "1-10": string,
+    "11-20": string,
+    "21-40": string,
+    "40+"?: string,
+  }
 }
+
+const getScoreToPar = (player: Player) =>
+  player.statistics.find((stat) => stat.name === 'scoreToPar')?.value || 0;
 
 export default async function Home() {
   const result = await fetch('https://site.web.api.espn.com/apis/site/v2/sports/golf/leaderboard?league=pga&region=us&lang=en&event=401703511', {cache: 'no-store'});
@@ -24,24 +34,25 @@ export default async function Home() {
     [player.id]: player,
   }), {});
 
+  const calcOverallScore = (picks: Picker["picks"]) => {
+    const players = Object.values(picks).map((pick) => golfersIdMap[pick]);
+    const sortedPlayers = players.sort((playerA, playerB) => getScoreToPar(playerA) - getScoreToPar(playerB));
+    const scoredPlayers = sortedPlayers.slice(0, 3);
+    return scoredPlayers.reduce((sum, player) => sum + getScoreToPar(player), 0);
+  }
+
   return (
     <div className="p-4">
-      {/* {golfers.map((player) => (
-        <div className="flex">
-          <div>{player.score}</div>
-          <div>{player.athlete.fullName}</div>
-        </div>
-      ))} */}
       {picks
         .sort((a_pick, b_pick) => (
-          Object.values(a_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0) -  Object.values(b_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0)
+          calcOverallScore(a_pick.picks) - calcOverallScore(b_pick.picks)
         ))
         .map((pick, i) => (
         <div className="mb-8 max-w-2xl mx-auto" key={`${pick.id}-${pick.name.replace(' ', '-')}`}>
           <div className='flex gap-4 p-4 bg-emerald-400 text-black font-bold rounded-t-2xl border-2 border-emerald-950'>
             <div className="shrink-0 grow-0">{i + 1}</div>
             <div className='text-center flex-grow'>{pick.name}</div>
-            <div className="shrink-0 grow-0">Overall: {parseScore(Object.values(pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0))}</div>
+            <div className="shrink-0 grow-0">Overall: {calcOverallScore(pick.picks)}</div>
           </div>
           <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 border-2 border-emerald-950">
             <div className="bg-emerald-800 text-white">
@@ -57,10 +68,10 @@ export default async function Home() {
                 Overall
               </div>
           </div>
-          <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 [&>div]:bg-white  rounded-b-2xl border-2 border-emerald-950 overflow-hidden">
+          <div className="grid grid-cols-[1fr_50%_1fr_1fr] text-center gap-0.5 bg-emerald-950 [&>div]:bg-white [&>div:last-of-type]:bg-gray-300 rounded-b-2xl border-2 border-emerald-950 overflow-hidden">
               {Object
                 .values(pick.picks)
-                .sort((a_id, b_id) => scoreToInt(golfersIdMap[a_id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue) - scoreToInt(golfersIdMap[b_id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue))
+                .sort((a_id, b_id) => getScoreToPar(golfersIdMap[a_id]) - getScoreToPar(golfersIdMap[b_id]))
                 .map((id) => (
                   <>
                     <div className="p-2 text-ellipsis overflow-hidden grid items-center justify-center">{golfersIdMap[id].status.position.displayName}</div>
