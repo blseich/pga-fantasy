9888403474be473ea20e384be7581b38355a7b6f
Brandon Seich <blseich@gmail.com>
2025-05-16 10:02:33 -0400

    some style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function Home() {
           Object.values(a_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0) -  Object.values(b_pick.picks).reduce((acc, id) => acc + scoreToInt(golfersIdMap[id].statistics.find((stat: { name: string }) => stat.name === 'scoreToPar').displayValue), 0)
         ))
         .map((pick, i) => (
-        <div className="mb-8" key={`${pick.id}-${pick.name.replace(' ', '-')}`}>
+        <div className="mb-8 max-w-2xl mx-auto" key={`${pick.id}-${pick.name.replace(' ', '-')}`}>
           <div className='flex gap-4 p-4 bg-emerald-400 text-black font-bold rounded-t-2xl'>
             <div className="shrink-0 grow-0">{i + 1}</div>
             <div className='text-center flex-grow'>{pick.name}</div>
