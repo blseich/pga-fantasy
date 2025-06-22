94499bb0e772c5a33fba89012ce6b5ccc8f150f5
Brandon Seich <blseich@gmail.com>
2025-06-21 21:30:28 -0400

    disable tiebreaker editor when tournament is in progress

--- features/tiebreaker-view/components/tiebreaker-view.tsx
+++ features/tiebreaker-view/components/tiebreaker-view.tsx
@@ -10,14 +10,14 @@ export default async function TiebreakerView({
 }) {
   const tiebreakerScore = await getCurrentTiebreakerValue(public_id);
   const tournament = await getTournament();
-
+  console.log(tournament.tournamentStatus);
   return (
     <>
       <h1 className="mb-4 mt-8 text-center text-2xl font-black">Tiebreaker</h1>
       <div className="tiebreaker">
         <div className="mb-4 text-center">Winning Score to Par:</div>
         <div className="flex items-center justify-center gap-2">
-          {tournament.tournamentStatus !== 'NOT_STARTED' ? (
+          {tournament.tournamentStatus === 'NOT_STARTED' ? (
             <TiebreakerEditor initScore={tiebreakerScore} />
           ) : (
             <TiebreakerDisplay score={tiebreakerScore} />
